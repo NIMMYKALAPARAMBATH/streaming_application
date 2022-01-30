a7a42a9ab60b92d9bd90875e328747a9ab84133e
nimmy <nimmyjose1997@gmail.com>
2022-01-31 01:25:29 +0530

    Updates

---
+++ precache-manifest.9e553316f2b47c1327e1363dc07f00d5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbfb51fa03e77476ed13831708d3963",
+    "url": "/streaming_application/index.html"
+  },
+  {
+    "revision": "f3888c645b6128d93343",
+    "url": "/streaming_application/static/css/main.58d8b7dc.chunk.css"
+  },
+  {
+    "revision": "306ca5df1a06c8e9f50d",
+    "url": "/streaming_application/static/js/2.15005cda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/streaming_application/static/js/2.15005cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3888c645b6128d93343",
+    "url": "/streaming_application/static/js/main.a8c578ad.chunk.js"
+  },
+  {
+    "revision": "e73e3ba547a7f5e6326a",
+    "url": "/streaming_application/static/js/runtime-main.0cad50a4.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/streaming_application/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/streaming_application/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/streaming_application/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/streaming_application/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/streaming_application/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/streaming_application/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/streaming_application/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/streaming_application/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/streaming_application/static/media/poster9.65d6d500.jpg"
+  }
+]);
